f7b9eb5d899070e24b229f921d691745f4a4afc4
Stephen Paulger <stephenpaulger@users.noreply.github.com>
2023-04-02 16:03:44 +0100

    Fix for python 3

--- colours.py
+++ colours.py
@@ -11,25 +11,23 @@ from math import sqrt
 import random
 import argparse
 
-try:
-    import Image
-except ImportError:
-    from PIL import Image
+from PIL import Image
 
 
-Point = namedtuple('Point', ('coords', 'n', 'ct'))
-Cluster = namedtuple('Cluster', ('points', 'center', 'n'))
+Point = namedtuple("Point", ("coords", "n", "ct"))
+Cluster = namedtuple("Cluster", ("points", "center", "n"))
 
 
 def get_points(img):
     points = []
     w, h = img.size
     for count, color in img.getcolors(w * h):
-        points.append(Point(color, 3, count))
+        points.append(Point(color[:3], 3, count))
     return points
 
 
-rtoh = lambda rgb: '#%s' % ''.join(('%02x' % p for p in rgb))
+def rgb_to_hex(rgb):
+    return "#%02x%02x%02x" % tuple(rgb)
 
 
 def colorz(filename, n=3):
@@ -39,22 +37,20 @@ def colorz(filename, n=3):
     points = get_points(img)
     clusters = kmeans(points, n, 1)
     rgbs = [map(int, c.center.coords) for c in clusters]
-    return map(rtoh, rgbs)
+    return map(rgb_to_hex, rgbs)
 
 
 def euclidean(p1, p2):
-    return sqrt(sum([
-        (p1.coords[i] - p2.coords[i]) ** 2 for i in range(p1.n)
-    ]))
+    return sqrt(sum([(p1.coords[i] - p2.coords[i]) ** 2 for i in range(p1.n)]))
 
 
 def calculate_center(points, n):
-    vals = [0.0 for i in range(n)]
+    vals = [0.0] * n
     plen = 0
     for p in points:
         plen += p.ct
         for i in range(n):
-            vals[i] += (p.coords[i] * p.ct)
+            vals[i] += p.coords[i] * p.ct
     return Point([(v / plen) for v in vals], n, 1)
 
 
@@ -62,10 +58,11 @@ def kmeans(points, k, min_diff):
     clusters = [Cluster([p], p, p.n) for p in random.sample(points, k)]
 
     while 1:
-        plists = [[]] * k
+        plists = [[] for _ in range(k)]
 
         for p in points:
-            smallest_distance = float('Inf')
+            idx = 0
+            smallest_distance = float("Inf")
             for i in range(k):
                 distance = euclidean(p, clusters[i].center)
                 if distance < smallest_distance:
